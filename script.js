576ecbfce53aae6c26fa3bd05ee572da4a8e16fc
hackparking <155868720+hackparking@users.noreply.github.com>
2024-12-21 13:16:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('webhookForm').addEventListener('submit', function(event
     event.preventDefault(); // フォームのデフォルト動作を防ぐ
 
     const message = document.getElementById('message').value;
-    const webhookUrl = 'https://discord.com/api/webhooks/1278295313723166740/lZyRgGha8sxlfjdlYbfFXff2rvmP6HIhI96ziuBIQgFlrBfY_Yy9CpX8OJUDEe820O3K'; // ここにDiscord Webhook URLを入力
+    const webhookUrl = '？？'; // ここにDiscord Webhook URLを入力
 
     fetch(webhookUrl, {
         method: 'POST',
